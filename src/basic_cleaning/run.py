05d515bb5accc0e80d835fa16e059c20443f4485
Sinem <sinemunal199@gmail.com>
2023-04-30 16:39:03 +0200

    Limit lat and long feature values

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -32,6 +32,11 @@ def go(args):
 
     logging.info("Convert last_review to datetime")
     df['last_review'] = pd.to_datetime(df['last_review'])
+
+    logging.info("Limit longitude and latitude features")
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     logging.info("Save clean data")
     df.to_csv("clean_sample.csv", index=False)
 
